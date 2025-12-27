72a59d17b65be7cf93156ce1e7a200da45660335
GyeongHun <riverpower6@naver.com>
2025-12-27 18:42:09 +0900

    [FEAT] 로그유틸 추가 및 개발, 운영 서버 별로 application.yml 파일 분리

--- src/main/kotlin/io/river/back/domain/home/home/controller/HomeController.kt
+++ src/main/kotlin/io/river/back/domain/home/home/controller/HomeController.kt
@@ -1,5 +1,6 @@
 package io.river.back.domain.home.home.controller
 
+import io.river.back.standard.extensions.logger
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ResponseBody
@@ -8,11 +9,15 @@ import java.net.InetAddress
 @Controller
 class HomeController {
 
+    private val log = logger()
+
     @GetMapping
     @ResponseBody
     fun main(): String {
         val localhost = InetAddress.getLocalHost()
         val hostname = localhost.hostName
+
+        log.debug("개발/테스트 환경에서만 출력")
 
         return "main, hostname : $hostname"
     }
